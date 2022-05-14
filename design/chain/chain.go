7ceb9f4bf1f02c6c82d262c919ba47724642613d
tmaker <you@example.com>
2022-05-14 21:31:07 +0800

    debug

--- design/chain/chain.go
+++ design/chain/chain.go
@@ -1,1 +1,47 @@
 package chain
+
+// 责任链应用场景 敏感词过滤场景
+
+type ErrFilter interface {
+	Filter(content string) bool
+}
+
+type FilterA struct {
+}
+
+func (this *FilterA) Filter(content string) bool {
+	if content == "test" {
+		return false
+	}
+	return true
+}
+
+type FilterB struct {
+}
+
+func (this *FilterB) Filter(content string) bool {
+	if content == "test" {
+		return false
+	}
+	return true
+}
+
+// 核心业务责任链
+type FilterChain struct {
+	filters []ErrFilter
+}
+
+func (this *FilterChain) Add(f ErrFilter) {
+	this.filters = append(this.filters, f)
+}
+
+func (this *FilterChain) Filter(content string) bool {
+	if len(this.filters) > 0 {
+		for _, filter := range this.filters {
+			if !filter.Filter(content) {
+				return false
+			}
+		}
+	}
+	return true
+}
--- design/chain/chain_test.go
+++ design/chain/chain_test.go
@@ -1,1 +1,13 @@
 package chain
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test_Chain(t *testing.T) {
+	chain := new(FilterChain)
+	chain.Add(&FilterA{})
+	chain.Add(&FilterB{})
+	fmt.Println(chain.Filter("test"))
+}
